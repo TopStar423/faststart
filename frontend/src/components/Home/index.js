5321b2a0bad04f23b2c4407f51ac4792647d5e31
TopStar423 <topstar.superman@gmail.com>
2020-03-27 17:57:10 -0700

    Fix typo

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     render() {
         return (
             <div className="homepage">
-                Homepage Comming Soon
+                Homepage Coming Soon
             </div>
         )
     }
